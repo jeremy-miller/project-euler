e8e444fd8381687ed4b6df616e902583ea626e7a
Jeremy <jgmiller88@gmail.com>
2017-07-03 10:56:08 -0700

    pull solutionSet out into separate data structure, add problem 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,21 @@
 module Main where
 
-import qualified Data.Map as Map (fromList, lookup)
+import qualified Data.Map as Map (Map, fromList, lookup)
 import qualified Problem1 (solve)
+import qualified Problem2 (solve)
 import System.Environment (getArgs)
+
+solutionSet :: Map.Map Int Integer
+solutionSet = Map.fromList
+  [
+    (1, Problem1.solve)
+  , (2, Problem2.solve)
+  ]
 
 main :: IO ()
 main = do
   args <- getArgs
   let problemNumber = head args
-      solutionSet = Map.fromList [(1, Problem1.solve)]
   case Map.lookup (read problemNumber :: Int) solutionSet of
     Just solutionFunction -> return solutionFunction >>= print
     Nothing -> putStrLn $ "No solution exists for problem " ++ problemNumber
