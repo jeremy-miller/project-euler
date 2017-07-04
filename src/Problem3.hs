d282a2c95a089c607bcd35442d83051db50b576e
Jeremy <jgmiller88@gmail.com>
2017-07-03 22:23:21 -0700

    add solution for problem 3

--- src/Problem3.hs
+++ src/Problem3.hs
@@ -2,5 +2,11 @@ module Problem3
   ( solve
   ) where
 
+findLargestPrimeFactor :: Integer -> Integer -> Integer -> Integer
+findLargestPrimeFactor number currentDivisor largestDivisor
+  | number == 1 = largestDivisor
+  | number `mod` currentDivisor == 0 = findLargestPrimeFactor (number `div` currentDivisor) currentDivisor currentDivisor
+  | otherwise = findLargestPrimeFactor number (currentDivisor + 1) largestDivisor
+
 solve :: Integer
-solve = 3
+solve = findLargestPrimeFactor 600851475143 2 1
