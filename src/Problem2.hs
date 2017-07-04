e8c14429e405226d4898946f8dd988fe15503f3c
Jeremy <jgmiller88@gmail.com>
2017-07-03 21:55:58 -0700

    add comment to helper function of problem 2

--- src/Problem2.hs
+++ src/Problem2.hs
@@ -2,6 +2,11 @@ module Problem2
   ( solve
   ) where
 
+{-|
+  zipWith pulls out the 'head' of both the 'fibonacci' and 'tail fibonacci' lists (the existing list being build)
+  and adds them together.  It then appends the summed value to the end 'fibonacci' list.  This new valus is then used
+  in the next iteration of zipWith to calculate the next number in the sequence.
+-}
 fibonacci :: [Integer]
 fibonacci = 1 : 2 : zipWith (+) fibonacci (tail fibonacci)
 
