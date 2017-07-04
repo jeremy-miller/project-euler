9edb0f52306c8b1537606b4a9cae3baed037d3ce
Jeremy <jgmiller88@gmail.com>
2017-07-03 21:32:12 -0700

    solved problem 2

--- src/Problem2.hs
+++ src/Problem2.hs
@@ -2,5 +2,8 @@ module Problem2
   ( solve
   ) where
 
+fibonacci :: [Integer]
+fibonacci = 1 : 2 : zipWith (+) fibonacci (tail fibonacci)
+
 solve :: Integer
-solve = 2
+solve = sum [x | x <- takeWhile (<= 4000000) fibonacci, even x]
