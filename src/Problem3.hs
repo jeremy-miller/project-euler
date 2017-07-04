67096f041fcbe99246b4f925d64913183254c95b
Jeremy <jgmiller88@gmail.com>
2017-07-04 09:10:19 -0700

    separate out special case of 2 from rest, add comments

--- src/Problem3.hs
+++ src/Problem3.hs
@@ -4,9 +4,10 @@ module Problem3
 
 findLargestPrimeFactor :: Integer -> Integer -> Integer -> Integer
 findLargestPrimeFactor number currentDivisor largestDivisor
-  | number == 1 = largestDivisor
+  | number == 1 = largestDivisor  -- largest prime divisor found, so return
   | number `mod` currentDivisor == 0 = findLargestPrimeFactor (number `div` currentDivisor) currentDivisor currentDivisor
-  | otherwise = findLargestPrimeFactor number (currentDivisor + 1) largestDivisor
+  | currentDivisor == 2 = findLargestPrimeFactor number (currentDivisor + 1) largestDivisor
+  | otherwise = findLargestPrimeFactor number (currentDivisor + 2) largestDivisor  -- no need to checking even numbers after 2
 
 solve :: Integer
 solve = findLargestPrimeFactor 600851475143 2 1
