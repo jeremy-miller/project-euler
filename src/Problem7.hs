cc85dca2f1a1296c0d2c1dc0adb6417dc7e1b7da
Jeremy <jgmiller88@gmail.com>
2017-07-05 22:25:09 -0700

    work in progress

--- src/Problem7.hs
+++ src/Problem7.hs
@@ -2,5 +2,17 @@ module Problem7
   ( solve
   ) where
 
+-- | Upper bound calculated using n * log(n) (see https://primes.utm.edu/howmany.html)
+-- | If n = 10,1001, upper bound = 10,001 * log(10,001) = 92,113
+upperBound :: Integer
+upperBound = 1000000
+
+sieveOfEratosthenes :: [Integer] -> [Integer] -> Integer
+sieveOfEratosthenes processedList primesList
+  | currentPrime ^ 2 > upperBound = (primesList ++ processedList) !! 10001
+  | otherwise = sieveOfEratosthenes filteredList $ primesList ++ [currentPrime]
+  where currentPrime = head processedList
+        filteredList = filter (\x -> x `mod` currentPrime /= 0) processedList
+
 solve :: Integer
-solve = 7
+solve = sieveOfEratosthenes [3, 5 .. upperBound] [2]
