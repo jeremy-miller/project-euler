82b4f85a755cdb0a9b8b3d05a75ec38275ce2ff8
Jeremy <jgmiller88@gmail.com>
2017-07-06 21:32:53 -0700

    remove comment

--- src/Problem3.hs
+++ src/Problem3.hs
@@ -4,7 +4,7 @@ module Problem3
 
 findLargestPrimeFactor :: Integer -> Integer -> Integer -> Integer
 findLargestPrimeFactor number currentDivisor largestDivisor
-  | number == 1 = largestDivisor  -- largest prime divisor found, so return
+  | number == 1 = largestDivisor
   | number `mod` currentDivisor == 0 = findLargestPrimeFactor (number `div` currentDivisor) currentDivisor currentDivisor
   | currentDivisor == 2 = findLargestPrimeFactor number (currentDivisor + 1) largestDivisor
   | otherwise = findLargestPrimeFactor number (currentDivisor + 2) largestDivisor  -- no need to checking even numbers after 2
