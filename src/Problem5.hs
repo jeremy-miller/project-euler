eb66e512158e83b726d73dbfe7210d603359468f
Jeremy <jgmiller88@gmail.com>
2017-07-06 21:32:24 -0700

    remove comment

--- src/Problem5.hs
+++ src/Problem5.hs
@@ -4,7 +4,7 @@ module Problem5
 
 smallestMultiple :: Integer -> Integer -> Integer
 smallestMultiple number divisor
-  | divisor == 1 = number  -- return the answer
+  | divisor == 1 = number
   | number `mod` divisor == 0 = smallestMultiple number $ divisor - 1
   | otherwise = smallestMultiple (number + 20) 19  -- add 20 so we know it's at least divisible by 20
 
