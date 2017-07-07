14561a62d450ad16a7d2aab006ce01139a181d39
Jeremy <jgmiller88@gmail.com>
2017-07-06 18:20:54 -0700

    updates from hindent

--- src/Problem7.hs
+++ src/Problem7.hs
@@ -6,8 +6,9 @@ sieveOfEratosthenes :: [Integer] -> [Integer] -> Integer
 sieveOfEratosthenes processedList primesList
   | length primesList >= 10001 = last primesList
   | otherwise = sieveOfEratosthenes filteredList $ primesList ++ [currentPrime]
-  where currentPrime = head processedList
-        filteredList = filter (\x -> x `mod` currentPrime /= 0) processedList
+  where
+    currentPrime = head processedList
+    filteredList = filter (\x -> x `mod` currentPrime /= 0) processedList
 
 solve :: Integer
-solve = sieveOfEratosthenes [3, 5 ..] [2]
+solve = sieveOfEratosthenes [3,5 ..] [2]
