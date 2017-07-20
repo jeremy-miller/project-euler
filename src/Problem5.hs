862873a5c51768a5778cb539c6a4627bae0d47f1
Jeremy Miller <jgmiller88@gmail.com>
2017-07-19 20:04:24 -0700

    add doctest

--- src/Problem5.hs
+++ src/Problem5.hs
@@ -8,5 +8,9 @@ smallestMultiple number divisor
   | number `mod` divisor == 0 = smallestMultiple number $ divisor - 1
   | otherwise = smallestMultiple (number + 20) 19  -- add 20 so we know it's at least divisible by 20
 
+-- | What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+--
+-- >>> solve
+-- 232792560
 solve :: Integer
 solve = smallestMultiple 20 19
