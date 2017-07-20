908c92bc8333021829488f8d161639dce96db808
Jeremy Miller <jgmiller88@gmail.com>
2017-07-19 19:54:04 -0700

    add doctest

--- src/Problem1.hs
+++ src/Problem1.hs
@@ -2,5 +2,9 @@ module Problem1
   ( solve
   ) where
 
+-- | Find the sum of all the multiples of 3 or 5 below 1000.
+--
+-- >>> solve
+-- 233168
 solve :: Integer
 solve = sum [x | x <- [3 .. 999], x `mod` 3 == 0 || x `mod` 5 == 0]
