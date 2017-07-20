84ab57d3ffb5948b14351cf35dc5b10d3bb2c89a
Jeremy Miller <jgmiller88@gmail.com>
2017-07-19 20:05:24 -0700

    add doctest

--- src/Problem6.hs
+++ src/Problem6.hs
@@ -2,6 +2,11 @@ module Problem6
   ( solve
   ) where
 
+-- | Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+--
+-- Example:
+-- >>> solve
+-- 25164150
 solve :: Integer
 solve = squareOfSum - sumOfSquares
   where squareOfSum = sum [1 .. 100] ^ 2
