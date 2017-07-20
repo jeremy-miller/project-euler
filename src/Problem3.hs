f537bf29bf693f9a317ddc634a351e573de22e46
Jeremy Miller <jgmiller88@gmail.com>
2017-07-19 20:00:32 -0700

    add doctest

--- src/Problem3.hs
+++ src/Problem3.hs
@@ -9,5 +9,10 @@ findLargestPrimeFactor number currentDivisor largestDivisor
   | currentDivisor == 2 = findLargestPrimeFactor number (currentDivisor + 1) largestDivisor
   | otherwise = findLargestPrimeFactor number (currentDivisor + 2) largestDivisor  -- no need to checking even numbers after 2
 
+-- | What is the largest prime factor of the number 600851475143?
+--
+-- Example:
+-- >>> solve
+-- 6857
 solve :: Integer
 solve = findLargestPrimeFactor 600851475143 2 1
