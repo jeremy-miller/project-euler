bd920fd4b11ca8d9ae5a151e94b750d5c41d6ade
Jeremy Miller <jgmiller88@gmail.com>
2017-07-19 20:01:59 -0700

    add doctest

--- src/Problem4.hs
+++ src/Problem4.hs
@@ -2,5 +2,10 @@ module Problem4
   ( solve
   ) where
 
+-- | Find the largest palindrome made from the product of two 3-digit numbers.
+--
+-- Example
+-- >>> solve
+-- 906609
 solve :: Integer
 solve = maximum [number | x <- [100 .. 999], y <- [x .. 999], let number = x * y, show number == reverse (show number)]
